3c1f61490ea429ad934c67e1893ea5080b0c499d
Chgaurav36 <gauravchaurasia619@gmail.com>
2024-01-07 13:43:27 +0530

    added a select item for filtration of product

--- E-commerce Store/src/context/filterContext.js
+++ E-commerce Store/src/context/filterContext.js
@@ -7,7 +7,8 @@ const FilterContext = createContext();
 const initialState = {
   filter_products: [],
   all_products: [],
-  grid_view: true,
+  grid_view: false,
+  sorting_value: "lowest",
 };
 
 export const FilterContextProvider = ({ children }) => {
@@ -20,13 +21,26 @@ export const FilterContextProvider = ({ children }) => {
   const setGridView = () => {
     return dispatch({ type: "SET_GRID_VIEW" });
   };
+   // to set grid view
+   const setListView = () => {
+    return dispatch({ type: "SET_LIST_VIEW" });
+  };
+
+//   sorting function
+const sorting = () =>{
+    return dispatch({ type: "SORT_ITEM" });
+}
+
+useEffect(()=>{
+    dispatch({ type: "SORTING_PRODUCTS", payload: products });
+},[state.sorting_value])
 
   useEffect(() => {
     dispatch({ type: "LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
   return (
-    <FilterContext.Provider value={{ ...state, setGridView }}>
+    <FilterContext.Provider value={{ ...state, setGridView, setListView, sorting }}>
       {children}
     </FilterContext.Provider>
   );
--- E-commerce Store/src/reducers/FilterReducer.js
+++ E-commerce Store/src/reducers/FilterReducer.js
@@ -12,9 +12,59 @@ const filterReducer = (state, action) => {
         ...state,
         grid_view: true,
       };
+
+      case "SET_LIST_VIEW":
+      return {
+        ...state,
+        grid_view: false,
+      };
+
+      case "SORT_ITEM":
+        let userSortValue = document.getElementById("sort");
+        let userValue  = userSortValue.options[userSortValue.selectedIndex].value;
+        console.log(userValue)
+      return {
+        ...state,
+        sorting_value: userValue,
+      };
+
+      case "SORTING_PRODUCTS":
+        let newSortData;
+        // let tempSortProduct = [...action.payload];
+  
+        const { filter_products, sorting_value } = state;
+        let tempSortProduct = [...filter_products];
+  
+        const sortingProducts = (a, b) => {
+          if (sorting_value === "lowest") {
+            return a.price - b.price;
+          }
+  
+          if (sorting_value === "highest") {
+            return b.price - a.price;
+          }
+  
+          if (sorting_value === "a-z") {
+            return a.name.localeCompare(b.name);
+          }
+  
+          if (sorting_value === "z-a") {
+            return b.name.localeCompare(a.name);
+          }
+        };
+  
+        newSortData = tempSortProduct.sort(sortingProducts);
+  
+        return {
+          ...state,
+          filter_products: newSortData,
+        };
+
+      default:
+        return state;
   }
 
-  return state;
+  
 };
 
 export default filterReducer;
